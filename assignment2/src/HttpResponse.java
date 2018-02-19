1ee953964d3f9aea67064c5668f181c444c7c12d
Alennartsson <al23sp@studnet.lnu.se>
2018-02-19 17:09:48 +0100

    Fixed index

--- assignment2/src/HttpResponse.java
+++ assignment2/src/HttpResponse.java
@@ -1,6 +1,4 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class HttpResponse {
 
@@ -11,23 +9,54 @@ public class HttpResponse {
 
 
     public HttpResponse(HttpRequest request, byte[] buf , int id) throws IOException {
-        try {
-            file = new FileInputStream(request.getFilePath());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("File not found for: "+id);
-        }
         int byteReader = 0;
         if(request.getFilePath().matches(".*.png") || request.getFilePath().matches(".*.ico") ){
+            try {
+                file = new FileInputStream(request.getFilePath());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File not found for: "+id);
+            }
+
             data = new byte[file.available()];
             file.read(data);
             isImage = true;
         }
-        else{
+        else if(request.getFilePath().matches(".*.html") || request.getFilePath().matches(".*.htm")){
+            try {
+                file = new FileInputStream(request.getFilePath());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File not found for: "+id);
+            }
+
             while ((byteReader = file.read(buf)) != -1){
                 setResponse(new String(buf, 0 , byteReader));
             }
             isImage = false;
+        } else{
+            String path = request.getFilePath();
+            if(!path.endsWith("/")){
+                path += "/";
+            }
+            File f = new File(path);
+            File[] matchingFiles = f.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.startsWith("index") || name.startsWith("Index") && name.endsWith(".html") || name.endsWith(".htm");
+                }
+            });
+
+            if(matchingFiles.length == 0){
+                System.out.println("cant find index file!");
+            }else {
+                System.out.println("test");
+                file = new FileInputStream(matchingFiles[0].getAbsolutePath());
+                while ((byteReader = file.read(buf)) != -1) {
+                    setResponse(new String(buf, 0, byteReader));
+                }
+                isImage = false;
+            }
         }
 
        file.close();
