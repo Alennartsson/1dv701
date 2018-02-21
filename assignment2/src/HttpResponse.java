8c4f0764a1bda08dd1a36b722ca11a317c73d916
Sandra <sk222sq@student.lnu.se>
2018-02-21 12:50:47 +0100

    comments

--- assignment2/src/HttpResponse.java
+++ assignment2/src/HttpResponse.java
@@ -9,51 +9,51 @@ public class HttpResponse {
     private FileInputStream file = null;
 
     public HttpResponse(HttpRequest request, byte[] buf , int id, Header header) throws IOException {
-        if(check403(request.getFilePath())){
+        if(check403(request.getFilePath())){        //Checks to see if the requested file is forbidden
             header.setHttpResponse("403");
-        }else if(check302(request.getFilePath())){
+        }else if(check302(request.getFilePath())){  //Redirects the user to another page
             header.setHttpResponse("302");
             header.setLocation("https://www.google.se/");
             is302 = true;
         }else {
             int byteReader = 0;
-            if (request.getFilePath().matches(".*.png") || request.getFilePath().matches(".*.ico")) {
+            if (request.getFilePath().matches(".*.png") || request.getFilePath().matches(".*.ico")) {       //if statement to see if the file is a png or ico
                 if (request.getFilePath().matches(".*.png")) {
-                    header.setType("image/png");
+                    header.setType("image/png");                        //sets the type to ico or png if it is
                 } else {
                     header.setType("image/ico");
                 }
                 try {
                     file = new FileInputStream(request.getFilePath());
 
-                    header.setHttpResponse("200");
+                    header.setHttpResponse("200");                      //if the file is available the header is set to response 200
                     data = new byte[file.available()];
                     file.read(data);
                     isImage = true;
                     header.setLength(getData().length);
-                } catch (FileNotFoundException e) {
+                } catch (FileNotFoundException e) {                     //sends 404 if file is not found
                     header.setHttpResponse("404");
                 }
             } else if (request.getFilePath().matches(".*.html") || request.getFilePath().matches(".*.htm")) {
                 try {
-                    file = new FileInputStream(request.getFilePath());
+                    file = new FileInputStream(request.getFilePath());          //else if the file is html or htm
                     header.setHttpResponse("200");
                     while ((byteReader = file.read(buf)) != -1) {
                         setResponse(new String(buf, 0, byteReader));
                     }
                     isImage = false;
 
-                } catch (FileNotFoundException e) {
+                } catch (FileNotFoundException e) {                 //sends 404 if file is not found
                     header.setHttpResponse("404");
                 }
 
             } else {
-                String path = request.getFilePath();
+                String path = request.getFilePath();                //if the requested file does not have a path check the directory
                 if (path.isEmpty()) {
                     path += System.getProperty("user.dir");
                 }
                 File f = new File(path);
-                File[] matchingFiles = f.listFiles(new FileFilter() {
+                File[] matchingFiles = f.listFiles(new FileFilter() {   //if the directory contains an index file, reads it
                     public boolean accept(File pathname) {
                         String name = pathname.getName().toLowerCase();
                         return name.startsWith("index") && name.endsWith(".html") || name.endsWith(".htm");
@@ -61,7 +61,7 @@ public class HttpResponse {
                 });
 
                 try{
-                    if (matchingFiles.length == 0) {
+                    if (matchingFiles.length == 0) {        //If the file is empty, sends 404
                         System.out.println("sadsaa");
                         header.setHttpResponse("404");
                         isImage = false;
--- assignment2/src/TCPEchoServer.java
+++ assignment2/src/TCPEchoServer.java
@@ -6,10 +6,10 @@ public class TCPEchoServer {
     public static final int MYPORT = 8888;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        ServerSocket conectionSocket = new ServerSocket(MYPORT);  //Create a socket with the used ip
+        ServerSocket socket = new ServerSocket(MYPORT);  //Create a socket with the used ip
         int id = 1; //Id that is used to give the clients id.
-        while (true) {      //While loop that waits for connection and starts a new thread if conneceted
-            Socket connectionSocket = conectionSocket.accept(); //Waits for a connection and creates a new socket for each connection
+        while (true) {      //While loop that waits for connection and starts a new thread if connected
+            Socket connectionSocket = socket.accept(); //Waits for a connection and creates a new socket for each connection
             System.out.println("Client ["+id+"] connected!");
             tcpClient client = new tcpClient(connectionSocket, id++);
             client.start();
@@ -33,14 +33,14 @@ class tcpClient extends Thread{
         byte[] buf= new byte[BUFSIZE];
         try{
             InputStream in = tcpSocket.getInputStream();      //InputStream that loads the message from the client
-            OutputStream out = tcpSocket.getOutputStream();      //OutputStrea that sends message to the client
+            OutputStream out = tcpSocket.getOutputStream();      //OutputStream that sends message to the client
             Header header = new Header();
-            //while(true){    //This loop should keep looping untill the client is disconnected.
+            //while(true){    //This loop should keep looping until the client is disconnected.
                 try {
                     try{
                         if(test500) { throw new IOException();}
                         int byteReader = in.read(buf);      //We read the received package from the client
-                        if (byteReader != -1) {     //If that package isnt lower than 0, lower than 0 means that it dont contain any message.Then we should load a new package.
+                        if (byteReader != -1) {     //If that package isn't lower than 0, lower than 0 means that it does not contain any message.Then we should load a new package.
                             finalMessage = new String(buf, 0, byteReader);      //Add that package to the string
                             HttpRequest request = new HttpRequest(finalMessage);
                             System.out.println("Filepath: "+request.getFilePath() +" requestType: " +request.getReq() +" HttpVersion: " +request.getVersion());
@@ -57,7 +57,7 @@ class tcpClient extends Thread{
                                 System.out.println("Response: "+header.getHttpResponse());
                                 out.write(header.getHttpResponse().getBytes());
                                 if(response.isIs302()){out.write(header.getLocation().getBytes());}
-                                out.write(response.getResponse().getBytes("UTF-8"));             //Send that message to the client, this is done untill the hole message is sent.
+                                out.write(response.getResponse().getBytes("UTF-8"));             //Send that message to the client, this is done until the hole message is sent.
                             }
                         }
                     }catch (IOException e){
@@ -66,7 +66,7 @@ class tcpClient extends Thread{
                     }
 
                 }catch (Exception e){
-                    System.out.printf("Client ["+clientId+"] disconnected ");       //If we disconnect in the middel of a transfer we need to break
+                    System.out.printf("Client ["+clientId+"] disconnected ");       //If we disconnect in the middle of a transfer we need to break
                    // break;
                 }
 
